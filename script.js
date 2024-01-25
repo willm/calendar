6d27b58f4b99d49545c4aad138b873d12d3b052d
Will Munn <william.munn@rubix.com>
2024-01-25 19:14:48 +0100

    Dynamically render calendar based on date

--- script.js
+++ script.js
@@ -1,26 +1,59 @@
-function highlightCurrentTime(today) {
-  const day = today.getDay() + 1;
-  const dayHeaderCells = document.querySelectorAll(
-    `thead > tr > th:nth-child(${day})`
-  );
-  const dayCells = document.querySelectorAll(
-    `tbody > tr > td:nth-child(${day})`
-  );
+import {Temporal} from '@js-temporal/polyfill';
+import {days, months} from './lib/constants.js';
+import {renderCalendar} from './lib/calendar-view.js';
 
+function registerAddCalendarButton() {
+  const dialog = document.getElementById('add-calendar-modal');
+  const closeButton = document.getElementById('close-calendar-modal');
+  closeButton.addEventListener('click', () => dialog.close());
   document
-    .querySelector(
-      `tbody > tr:nth-child(${today.getHours() + 1}) > td:nth-child(${day})`
-    )
-    .classList.toggle('current-time');
-  Array.from(dayHeaderCells)
-    .concat(Array.from(dayCells))
-    .forEach((e) => e.classList.toggle('current-day'));
+    .getElementById('add-calendar-button')
+    .addEventListener('click', () => {
+      dialog.showModal();
+    });
+
+  const form = document.getElementById('add-calendar-form');
+  form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    const link = document.getElementById('ical-link').value;
+    console.log(link);
+    const res = await fetch(link);
+    const icalData = res.text();
+    console.log(icalData);
+  });
+}
+
+function getData() {
+  const now = Temporal.Now.plainDateISO();
+  const dayOfMonth = now.toPlainMonthDay().day;
+  const dayOfWeek = days[now.dayOfWeek];
+  return {
+    weekDays: days.map((day, i) => {
+      return {
+        highlight: now.dayOfWeek - i === 0,
+        name:
+          day +
+          ' ' +
+          Temporal.Now.plainDateISO()
+            .subtract({days: now.dayOfWeek - i})
+            .toPlainMonthDay().day,
+      };
+    }),
+    dayOfWeek,
+    month: months[now.month - 1],
+    dayOfMonth,
+    year: now.year,
+    hours: Array.from({length: 24}, (_, i) => ({
+      name: i.toString().padStart(2, '0') + ':00',
+      highlight: i === new Date().getHours(),
+    })),
+  };
 }
 
 function main() {
-  const today = new Date();
-  highlightCurrentTime(today);
-  console.log(today.getHours());
+  registerAddCalendarButton();
+  renderCalendar(getData());
+  setInterval(() => renderCalendar(getData()), 10000);
 }
 
 main();

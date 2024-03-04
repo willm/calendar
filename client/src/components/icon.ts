e8301cce63ca502538b8d116271fc5a0a1fb4ee7
Will Munn <william.munn@rubix.com>
2024-03-04 21:11:30 +0100

    Fix hover style on svg buttons

--- client/src/components/icon.ts
+++ client/src/components/icon.ts
@@ -5,7 +5,7 @@ const icons: Record<string, string> = {
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    stroke="#ffffff"
+    stroke="currentColor"
   >
     <path
       d="M21 12C21 16.9706 16.9706 21 12 21C9.69494 21 7.59227 20.1334 6 18.7083L3 16M3 12C3 7.02944 7.02944 3 12 3C14.3051 3 16.4077 3.86656 18 5.29168L21 8M3 21V16M3 16H8M21 3V8M21 8H16"
@@ -18,8 +18,8 @@ const icons: Record<string, string> = {
     class="icon"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
-    stroke="#ffffff"
-    fill="ffffff"
+    stroke="currentColor"
+    fill="currentColor"
   >
     <path
       stroke-width="1.5"
@@ -65,10 +65,9 @@ class Icon extends HTMLElement {
 
   connectedCallback() {
     const attr = this.getAttribute('type');
-    const shadowRoot = this.attachShadow({mode: 'open'});
     const element = document.createElement('span');
     element.innerHTML = attr && icons[attr] ? icons[attr]! : '';
-    shadowRoot.appendChild(element);
+    this.appendChild(element);
   }
 }
 customElements.define('cal-icon', Icon);
